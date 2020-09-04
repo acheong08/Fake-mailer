f502a255b53e54377cf96b618abdb6ea4ddf1eba
Antonio <bitnuller101@gmail.com>
2020-09-04 13:34:36 +0800

    First Commit

---
+++ mail.php
@@ -0,0 +1,57 @@
+<?php 
+ 
+// Recipient 
+$to = $_POST["ToAddress"]; 
+ 
+// Sender 
+$from = $_POST["FromAddress"]; 
+$fromName = $_POST["FromName"]; 
+ 
+// Email subject 
+$subject = $_POST["Subject"];  
+ 
+// Attachment file 
+$file = 'uploads/' . $_POST["FilePath"]; 
+ 
+// Email body content 
+$htmlContent = $_POST["Body"]; 
+ 
+// Header for sender info 
+$headers = "From: $fromName"." <".$from.">"; 
+ 
+// Boundary  
+$semi_rand = md5(time());  
+$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x";  
+ 
+// Headers for attachment  
+$headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
+ 
+// Multipart boundary  
+$message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" . 
+"Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";  
+ 
+// Preparing attachment 
+if(!empty($file) > 0){ 
+    if(is_file($file)){ 
+        $message .= "--{$mime_boundary}\n"; 
+        $fp =    @fopen($file,"rb"); 
+        $data =  @fread($fp,filesize($file)); 
+ 
+        @fclose($fp); 
+        $data = chunk_split(base64_encode($data)); 
+        $message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" .  
+        "Content-Description: ".basename($file)."\n" . 
+        "Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" .  
+        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n"; 
+    } 
+} 
+$message .= "--{$mime_boundary}--"; 
+$returnpath = "-f" . $from; 
+ 
+// Send email 
+$mail = @mail($to, $subject, $message, $headers, $returnpath);  
+ 
+// Email sending status 
+echo $mail?"<h1>Email Sent Successfully!</h1>":"<h1>Email sending failed.</h1>"; 
+ 
+?>
